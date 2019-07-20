669b25c57491acbe054abe580b27aad97367a10b
Celal Cankaya <celalcankaya@outlook.com>
2019-07-20 16:13:19 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,10 +10,8 @@ class Anagram
   def match(toMatch)
     result = []
     toMatch.each do |doesItMatch|
-      puts doesItMatch.split('').sort
-      puts @words.split('').sort
       if @words.split('').sort == doesItMatch.split('').sort
-        result << toMatch
+        result << doesItMatch
       end
     end
     result
