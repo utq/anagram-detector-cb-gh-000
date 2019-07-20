b92f349a136efdafc755d5996345bdc1bed735c8
Celal Cankaya <celalcankaya@outlook.com>
2019-07-20 16:13:33 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :words
+
+  def initialize(words)
+    @words = words
+  end
+
+  def match(toMatch)
+    result = []
+    toMatch.each do |doesItMatch|
+      if @words.split('').sort == doesItMatch.split('').sort
+        result << doesItMatch
+      end
+    end
+    result
+  end
+end
