1b670879cfe19c55eb66a640dcb5851b69dcffc1
Celal Cankaya <celalcankaya@outlook.com>
2019-07-20 16:00:49 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ class Anagram
   end
   def match(toMatch)
     result = []
-    toMatch.each do |doesItMatch| 
+    toMatch.each do |doesItMatch|
       if %w(words) == %w(toMatch).sort
         result << toMatch
       end
